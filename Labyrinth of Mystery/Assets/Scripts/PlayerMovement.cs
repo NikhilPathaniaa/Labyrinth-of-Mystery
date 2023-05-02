2c96d369d6748a3ba90633eac122814e6cd56cb1
NikhilPathaniaa <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-05-02 19:29:08 +0530

    player movement

--- Labyrinth of Mystery/Assets/Scripts/PlayerMovement.cs
+++ Labyrinth of Mystery/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float dirx = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirx * 7f, rb.velocity.y);
+        
         if(Input.GetKeyDown("space"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
         }
     }
 }
