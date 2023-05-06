dd8073ba9209905ec6e860c8ae6319b684b07fde
NikhilPathaniaa <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-05-06 16:15:10 +0530

    Player flipping side ways animation
    here I do side flipping animation and my code DRY code. and simplify clean code

--- Labyrinth of Mystery/Assets/Scripts/PlayerMovement.cs
+++ Labyrinth of Mystery/Assets/Scripts/PlayerMovement.cs
@@ -4,37 +4,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
     private Animator anim;
+
+    private float dirx; // player side ways movement
+    [SerializeField] private float moveSpeed =7f;
+    [SerializeField] private float jumpSpeed =14f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dirx = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirx * 7f, rb.velocity.y);
+        dirx = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirx * moveSpeed, rb.velocity.y);
         
         if(Input.GetKeyDown("space"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 14f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
 
+        UpdateAnimationState();
+    }
+    // UpdateAnimationUpdate is called for animation
+    public void UpdateAnimationState()
+    {
         if(dirx > 0f)
         {
             anim.SetBool("running",true);
+            sprite.flipX =false;
         }
         else if(dirx < 0f)
         {
             anim.SetBool("running",true);
+            sprite.flipX =true;
         }
         else
         {
             anim.SetBool("running",false);
-        }
+        }   
     }
 }
