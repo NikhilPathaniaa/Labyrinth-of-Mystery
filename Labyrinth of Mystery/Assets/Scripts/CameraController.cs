e617fde629cb9fad2ed851714bffd27864db6cf7
NikhilPathaniaa <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-05-09 14:53:59 +0530

    camera setup to all new characters
    virtual guy
    pink man
    mask dude
    ninja frog

--- Labyrinth of Mystery/Assets/Scripts/CameraController.cs
+++ Labyrinth of Mystery/Assets/Scripts/CameraController.cs
@@ -6,10 +6,14 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform pink_Man;
+    [SerializeField] private Transform ninja_frog;
+    [SerializeField] private Transform mask_dude;
     // Update is called once per frame
     public void Update()
     {
         transform.position =  new Vector3(player.position.x, player.position.y, transform.position.z);  
-        transform.position =  new Vector3(pink_Man.position.x, pink_Man.position.y, transform.position.z); 
+        transform.position =  new Vector3(pink_Man.position.x, pink_Man.position.y, transform.position.z);
+        transform.position =  new Vector3(ninja_frog.position.x, ninja_frog.position.y, transform.position.z);  
+        transform.position =  new Vector3(mask_dude.position.x, mask_dude.position.y, transform.position.z);   
     }
 }
