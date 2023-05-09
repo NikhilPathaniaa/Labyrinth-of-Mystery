7100297790263fb3fc88b18e196db14c25060b3c
NikhilPathaniaa <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-05-09 14:51:16 +0530

    Pink Man is now completed
    2nd character is ready

--- Labyrinth of Mystery/Assets/Scripts/CameraController.cs
+++ Labyrinth of Mystery/Assets/Scripts/CameraController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private Transform pink_Man;
     // Update is called once per frame
     public void Update()
     {
-        transform.position =  new Vector3(player.position.x, player.position.y, transform.position.z);   
+        transform.position =  new Vector3(player.position.x, player.position.y, transform.position.z);  
+        transform.position =  new Vector3(pink_Man.position.x, pink_Man.position.y, transform.position.z); 
     }
 }
